95dbf33b4a16f40fcef65184eddb3eb84d244c59
Gustavo Lee <gglee89@gmail.com>
2020-10-28 21:20:19 +0900

    feat: Include 'react-router-dom' and 'react-router-bootstrap'

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ ECommerce Shop project with MERN
 
 1. Setup 'create-react-app'
 2. Import 'bootstrap.min.css' `Lux (A touch of glass)` theme from https://bootswatch.com/
-3. Install `react-bootstrap`
+3. Install `npm i react-bootstrap`
 4. Import `Nav` from `react-bootstrap` components.
 5. Import `font-awesome` CDN from cdnjs.com
+6. Install `npm i react-router-dom react-router-bootstrap`
 
 # Author
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Styles
 import { Container } from 'react-bootstrap';
 
 // Components
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
